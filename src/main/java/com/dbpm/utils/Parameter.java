75c7dd34e7ea4bc82aa890bb9196f61dbf64ecfa
gvenzl <gerald.venzl@gmail.com>
2016-03-27 17:44:16 -0700

    Missing Parameters file

---
+++ src/main/java/com/dbpm/utils/Parameter.java
@@ -0,0 +1,24 @@
+/*
+*
+* author:  gvenzl
+* created: 27 Mar 2016
+*
+* name: Parameter.java
+*
+*/
+
+package com.dbpm.utils;
+
+public final class Parameter {
+
+	public static String USER 			= "-user";
+	public static String ADMINUSER 		= "-adminUser";
+	public static String PASSWORD 		=  "-password";
+	public static String ADMINPASSWORD 	= "-adminPassword";
+	public static String HOST 			= "-host";
+	public static String PORT 			= "-port";
+	public static String DBNAME 		= "-dbName";
+	public static String VERBOSE		= "-v";
+	public static String PLATFORM 		= "-platform";
+
+}
