f3254f020fb192cb97cffa2498d70f58e6907133
gvenzl <gerald.venzl@gmail.com>
2017-11-06 20:26:59 -0800

    Add Logger unit tests.

---
+++ src/test/java/com/dbpm/logger/LoggerTest.java
@@ -0,0 +1,37 @@
+/*
+*
+* author:  gvenzl
+* created: 6 Nov 2017
+*
+* name: LoggerTest.java
+*
+*/
+
+package com.dbpm.logger;
+
+import org.junit.Test;
+
+public class LoggerTest {
+
+	@Test
+	public void test_log() {
+		Logger.log("Test message");
+	}
+	
+	@Test
+	public void test_error() {
+		Logger.error("Test error message");
+	}
+
+	@Test
+	public void test_verbose() {
+		Verbose.getInstance().setVerbose(true);
+		Logger.verbose("Verbose message, printed!");
+	}
+	
+	@Test
+	public void test_verboseDontPrint() {
+		Verbose.getInstance().setVerbose(false);
+		Logger.verbose("Verbose message, not printed!");
+	}
+}
