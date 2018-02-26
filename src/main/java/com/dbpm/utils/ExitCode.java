0973ee838a733b31e2c5274b3715e6b01a9df80b
gvenzl <gerald.venzl@gmail.com>
2018-02-25 19:03:52 -0800

    Javadoc
    Signed-off-by: gvenzl <gerald.venzl@gmail.com>

--- src/main/java/com/dbpm/utils/ExitCode.java
+++ src/main/java/com/dbpm/utils/ExitCode.java
@@ -15,21 +15,51 @@ package com.dbpm.utils;
 public enum ExitCode {
 
     // General exit codes
+    /**
+     * Successful program termination.
+     */
     EXIT_SUCCESSFUL(0),
+    /**
+     * An error has occurred, usually caused by some exception within libraries.
+     */
     EXIT_ERROR(1),
 
     // Build related codes: 101 - 199
+    /**
+     * The manifest was not found.
+     */
     EXIT_BUILD_MANIFEST_NOT_FOUND(101),
+    /**
+     * The manifest was not readable.
+     */
     EXIT_BUILD_MANIFEST_NOT_READABLE(102),
+    /**
+     * The manifest was not a valid JSON document.
+     */
     EXIT_BUILD_MANIFEST_NOT_VALID(103),
+    /**
+     * The package already exists and couldn't be overwritten.
+     */
     EXIT_BUILD_PACKAGE_EXISTS_CANT_OVERRIDE(121),
 
     // Config related codes: 201 - 299
+    /**
+     * The configuration could not be removed.
+     */
     EXIT_CONFIG_CANT_REMOVE_CONFIG(201),
+    /**
+     * The configuration could not be created.
+     */
     EXIT_CONFIG_CANT_CREATE_CONFIG(202),
 
     // Install related codes: 301 - 399
+    /**
+     * The package could not be installed.
+     */
     EXIT_INSTALL_COULD_NOT_INSTALL_PACKAGE(301),
+    /**
+     * The package to be installed could not be read.
+     */
     EXIT_INSTALL_CANT_READ_PACKAGE(302);
 
     // Uninstall related codes: 401 = 499
